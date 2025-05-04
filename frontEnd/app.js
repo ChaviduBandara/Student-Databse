8f40aece9e7ab2130b34d2dc61f18b678c619671
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2025-05-04 20:53:20 +0530

    updated code

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -10,24 +10,62 @@ function addStudent() {
     console.log("Age: " + age);
 
 
-} 
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "id": id,
+        "name": name,
+        "address": address,
+        "age": age
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+    };
+
+    fetch("http://localhost:8080/add-data", requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            loadTable();
+        })
+        .catch((error) => console.log(error));
+
+    loadTable();    
+}
 
 
-let table = document.getElementById("tblStudents");
+function loadTable() {
+    let table = document.getElementById("tblStudents");
+    
+    table.innerHTML=""
+    
 
-let body = "";
+    let body = `<tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Age</th>
+            </tr>`;
 
-fetch("http://localhost:8080/get-data")
-.then(res => res.json())
-.then(data => {
-    data.forEach(element => {
-        body += `<tr>
+    fetch("http://localhost:8080/get-data")
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(element => {
+                body += `<tr>
                     <td>${element.id}</td>
                     <td>${element.name}</td>
                     <td>${element.address}</td>
                     <td>${element.age}</td>
                 </tr>`
-    });
+            });
 
-    table.innerHTML = body;
-})
+            table.innerHTML = body;
+        })
+
+}
+
+
+
